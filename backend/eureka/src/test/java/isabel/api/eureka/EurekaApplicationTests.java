2a1f64ca828015bd0be7fc7208d8a2f4be13f4bd
lenyn15 <201514029@uns.edu.pe>
2021-03-24 22:46:16 -0500

    Configuration added

---
+++ backend/eureka/src/test/java/isabel/api/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package isabel.api.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
